8fc17854482e8acaac3005333baef58f40ac3d7a
Mohit Saini <mohitsainiknl2@gmail.com>
2024-01-23 23:31:46 +0530

    Fixed static log members undeclared

--- Tiler/Source/Log.cpp
+++ Tiler/Source/Log.cpp
@@ -3,6 +3,9 @@
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace Tiler {
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
 	void Log::init() {
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 		s_CoreLogger = spdlog::stdout_color_mt("TILER");
